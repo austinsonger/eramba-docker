1061ad4d60212defef592fee4ce478210a8572c0
austinsonger <austin@songer.pro>
2021-10-08 09:13:59 -0500

    Update

--- app/cron/eramba_hourly.sh
+++
@@ -1,3 +0,0 @@
-docker exec -i Eramba bash -c '/var/www/html/app/Console/cake cron job hourly'
-docker exec -i Eramba bash -c 'chown -R www-data:www-data /var/www/html/'
-docker exec -i Eramba bash -c 'chmod 777 -R /var/www/html/app/'
